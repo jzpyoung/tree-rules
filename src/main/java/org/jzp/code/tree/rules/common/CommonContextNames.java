ac58b653260869925e298088b098a8f45bbf5754
jzpyoung <jzpyoung@gmail.com>
2016-12-29 09:54:18 +0800

    mac test

--- src/main/java/org/jzp/code/tree/rules/common/CommonContextNames.java
+++ src/main/java/org/jzp/code/tree/rules/common/CommonContextNames.java
@@ -13,46 +13,47 @@ import org.jzp.code.tree.rules.context.Context.Name;
  * @date 2016-11-14
  */
 public class CommonContextNames {
-	
-	/**
-	 * 执行结果标识的上下文Name(out)
-	 */
-	public static final Name<Boolean> NAME_OUT_SUCCESS_FLAG = Name.create("out_success_flag");
-	
-	/**
-	 * 错误码的上下文Name(out)
-	 */
-	public static final Name<Integer> NAME_OUT_ERR_CODE = Name.create("out_err_code");
-	
-	/**
-	 * 错误信息的上下文Name(out)
-	 */
-	public static final Name<String> NAME_OUT_ERR_MESSAGE = Name.create("out_err_msg");
-	
-	/**
-	 * ToolCode在上下文中的Name(in)
-	 */
-	public static final Name<String> NAME_IN_TOOL_CODE = Name.create("in_tool_code");
-	
-	/**
-	 * 工具参数上下文在上下文中的Name(in)
-	 */
-	public static final Name<Context> NAME_IN_TOOL_PARAM_CONTEXT = Name.create("in_tool_param_context");
-	
-	/**
-	 * 资源ID在上下文中的Name(in)
-	 */
-	public static final Name<String> NAME_IN_RESOURCE_ID = Name.create("in_resource_id");
 
-	/**
-	 * 测试上下文的Name(in)
-	 */
-	public static final Name<String> NAME_IN_TEST = Name.create("in_test");
+    /**
+     * 执行结果标识的上下文Name(out)
+     */
+    public static final Name<Boolean> NAME_OUT_SUCCESS_FLAG = Name.create("out_success_flag");
 
-	/**
-	 * 测试上下文的Name(out)
-	 */
-	public static final Name<String> NAME_OUT_TEST = Name.create("in_out");
-	
-	private CommonContextNames(){}
+    /**
+     * 错误码的上下文Name(out)
+     */
+    public static final Name<Integer> NAME_OUT_ERR_CODE = Name.create("out_err_code");
+
+    /**
+     * 错误信息的上下文Name(out)
+     */
+    public static final Name<String> NAME_OUT_ERR_MESSAGE = Name.create("out_err_msg");
+
+    /**
+     * ToolCode在上下文中的Name(in)
+     */
+    public static final Name<String> NAME_IN_TOOL_CODE = Name.create("in_tool_code");
+
+    /**
+     * 工具参数上下文在上下文中的Name(in)
+     */
+    public static final Name<Context> NAME_IN_TOOL_PARAM_CONTEXT = Name.create("in_tool_param_context");
+
+    /**
+     * 资源ID在上下文中的Name(in)
+     */
+    public static final Name<String> NAME_IN_RESOURCE_ID = Name.create("in_resource_id");
+
+    /**
+     * 测试上下文的Name(in)
+     */
+    public static final Name<String> NAME_IN_TEST = Name.create("in_test");
+
+    /**
+     * 测试上下文的Name(out)
+     */
+    public static final Name<String> NAME_OUT_TEST = Name.create("in_out");
+
+    private CommonContextNames() {
+    }
 }
